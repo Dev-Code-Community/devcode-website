b9cbffab6718089a961598728c31872c99bbbb9c
Shishank <shishankrawat93774@gmail.com>
2023-08-03 13:11:01 +0530

    added header, footer. change in home for checking.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import About from "./pages/About";
 import Events from "./pages/Events";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/events' element={<Events />} />
-			</Routes>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,51 @@
 import "./Navbar.css";
 
 function Navbar() {
-	return <div>My Navbar</div>;
+  return (
+    <div className="header">
+      <div className="header-upper">
+        {" "}
+        <span className="icon-star">✦</span>{" "}
+        <span className="header-upper-text">
+          {" "}
+          Join the community of developers{" "}
+          <span className="text-special">dev code</span>, and Grow your skills,
+          build your network{" "}
+        </span>{" "}
+        <span className="icon-star">✦</span>{" "}
+      </div>
+      <div className="header-lower">
+        <div className="logo-container">
+          <img id="logo" src="src\assets\logo.png" alt="logo" />
+        </div>
+        <ul className="nav-links">
+          <li id="home-link" className="nav-link">
+            {" "}
+            <a href=""> Home </a>{" "}
+          </li>
+          <li className="nav-link">
+            {" "}
+            <a href="#about"> About Us </a>{" "}
+          </li>
+          <li className="nav-link">
+            {" "}
+            <a href=""> Benefits </a>{" "}
+          </li>
+          <li className="nav-link">
+            {" "}
+            <a href=""> Events </a>{" "}
+          </li>
+          <li className="nav-link">
+            {" "}
+            <a href=""> Community </a>{" "}
+          </li>
+        </ul>
+        <div className="nav-button">
+          <button>Let's Connect</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
